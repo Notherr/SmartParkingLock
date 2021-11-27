8b7b651cdf7f4f6ce4bd60cbdf13700dd1fcc820
seongkyu-lim <96x60812@naver.com>
2021-11-27 22:42:04 +0900

    [Refactor] url

--- Gateway/Gateway.ino
+++ Gateway/Gateway.ino
@@ -8,8 +8,8 @@ WiFiClient wifiClient;
 //const char* ssid = "jdhaun6F(2g)";
 //const char* password = "jdhaun2468";
 
-const char* ssid = "KT_GIGA_5G_WAVE2_13B7";
-const char* password = "cg87ff5116";
+const char* ssid = "seongkyu";
+const char* password = "38906081";
 
 void setup () {
 
@@ -20,6 +20,7 @@ void setup () {
     delay(3000);
     Serial.print("Connecting..");
   }
+  Serial.print("Connected!");
 
 }
 
@@ -28,20 +29,15 @@ void loop() {
   if (WiFi.status() == WL_CONNECTED) {
     HTTPClient http;
     int parkingLotId = 1;
-    String url = "ec2-3-37-183-92.ap-northeast-2.compute.amazonaws.com:8080/api_v1/checking/" + String(parkingLotId);
+    String url = "http://ec2-3-37-183-92.ap-northeast-2.compute.amazonaws.com:8080/api_v1/check?id=" + String(parkingLotId);
     http.begin(wifiClient, url);
     int httpCode = http.GET();                   
     if (httpCode == 200) {
       String payload = http.getString();
       Serial.println(payload);
-      digitalWrite(LED_BUILTIN, HIGH);
-      delay(100);
-      digitalWrite(LED_BUILTIN, LOW);
-      delay(100);
-      digitalWrite(LED_BUILTIN, HIGH);
-      delay(100);
-      digitalWrite(LED_BUILTIN, LOW);
-      delay(100);
+    }else{
+      String payload = "false";
+      Serial.println(payload);
     }
 
     http.end();
