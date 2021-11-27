9b48bbc9abbf04c2b6a969fa2f7e198a71564fcd
seongkyu-lim <96x60812@naver.com>
2021-11-27 16:16:11 +0900

    [Refactor] connecting wifi

--- Gateway/Gateway.ino
+++ Gateway/Gateway.ino
@@ -1,27 +1,30 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
+#include <Wire.h>
 #include <WiFiClient.h>
 
 WiFiClient wifiClient;
 
+//const char* ssid = "jdhaun6F(2g)";
+//const char* password = "jdhaun2468";
+
 const char* ssid = "KT_GIGA_5G_WAVE2_13B7";
 const char* password = "cg87ff5116";
 
+void setup () {
 
-void setup() {
   Serial.begin(115200);
-
   WiFi.begin(ssid, password);
+
   while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+    delay(3000);
+    Serial.print("Connecting..");
   }
-  Serial.println("");
-  Serial.println("WiFi connected");
 
 }
 
 void loop() {
+
   if (WiFi.status() == WL_CONNECTED) {
     HTTPClient http;
     int parkingLotId = 1;
@@ -31,12 +34,18 @@ void loop() {
     if (httpCode == 200) {
       String payload = http.getString();
       Serial.println(payload);
-  
+      digitalWrite(LED_BUILTIN, HIGH);
+      delay(100);
+      digitalWrite(LED_BUILTIN, LOW);
+      delay(100);
+      digitalWrite(LED_BUILTIN, HIGH);
+      delay(100);
+      digitalWrite(LED_BUILTIN, LOW);
+      delay(100);
     }
-  
+
     http.end();
   }
 
-  delay(1000);
-
+  delay(10000);
 }
